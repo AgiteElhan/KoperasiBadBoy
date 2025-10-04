d36bdf730514ae63a45b66562f418a4bd4899291
elhannnn <agit02394@gmail.com>
2025-10-04 09:30:05 +0700

    agit

---
+++ Forms/InstallmentForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KoperasiBadBoy.Forms
+{
+    public partial class InstallmentForm : Form
+    {
+        public InstallmentForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InstallmentForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
