f8fb20a61e2b1ea680f54a96b1ee26d4dc4f9f96
Adyl <adylpanduaja@gmail.com>
2025-10-04 09:29:54 +0700

    ll

---
+++ Forms/RegistrasiFrom.Designer.cs
@@ -0,0 +1,305 @@
+﻿    namespace KoperasiBadBoy.Forms
+{
+    partial class RegistrasiFrom
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox12 = new TextBox();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            textBox4 = new TextBox();
+            textBox9 = new TextBox();
+            textBox10 = new TextBox();
+            textBox13 = new TextBox();
+            textBox6 = new TextBox();
+            textBox1 = new TextBox();
+            textBox5 = new TextBox();
+            textBox7 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(101, 40);
+            label1.Name = "label1";
+            label1.Size = new Size(80, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Full Name ";
+            label1.Click += label1_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(365, 272);
+            label3.Name = "label3";
+            label3.Size = new Size(126, 20);
+            label3.TabIndex = 2;
+            label3.Text = "What Artis/Singer\r\n";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(85, 272);
+            label4.Name = "label4";
+            label4.Size = new Size(113, 20);
+            label4.TabIndex = 3;
+            label4.Text = "What Fav Color ";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(401, 106);
+            label5.Name = "label5";
+            label5.Size = new Size(54, 20);
+            label5.TabIndex = 4;
+            label5.Text = "Phone ";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(116, 179);
+            label6.Name = "label6";
+            label6.Size = new Size(50, 20);
+            label6.TabIndex = 5;
+            label6.Text = "Email ";
+            label6.Click += label6_Click;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(112, 106);
+            label7.Name = "label7";
+            label7.Size = new Size(59, 20);
+            label7.TabIndex = 8;
+            label7.Text = "Card ID";
+            label7.Click += label7_Click;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(389, 40);
+            label8.Name = "label8";
+            label8.Size = new Size(79, 20);
+            label8.TabIndex = 7;
+            label8.Text = "Username ";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(679, 186);
+            label9.Name = "label9";
+            label9.Size = new Size(56, 20);
+            label9.TabIndex = 6;
+            label9.Text = "Adders";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(390, 179);
+            label10.Name = "label10";
+            label10.Size = new Size(76, 20);
+            label10.TabIndex = 11;
+            label10.Text = "Referce Id";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(670, 47);
+            label11.Name = "label11";
+            label11.Size = new Size(74, 20);
+            label11.TabIndex = 10;
+            label11.Text = "Password ";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(668, 113);
+            label12.Name = "label12";
+            label12.Size = new Size(78, 20);
+            label12.TabIndex = 9;
+            label12.Text = "Phone ALT";
+            label12.Click += label12_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(578, 70);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(258, 27);
+            textBox2.TabIndex = 13;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(578, 154);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(258, 27);
+            textBox3.TabIndex = 14;
+            // 
+            // textBox12
+            // 
+            textBox12.Location = new Point(578, 217);
+            textBox12.Multiline = true;
+            textBox12.Name = "textBox12";
+            textBox12.Size = new Size(258, 117);
+            textBox12.TabIndex = 23;
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.ImageScalingSize = new Size(20, 20);
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(61, 4);
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(299, 70);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(258, 27);
+            textBox4.TabIndex = 25;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(299, 217);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(258, 27);
+            textBox9.TabIndex = 26;
+            textBox9.TextChanged += textBox9_TextChanged;
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(299, 307);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(258, 27);
+            textBox10.TabIndex = 27;
+            // 
+            // textBox13
+            // 
+            textBox13.Location = new Point(299, 154);
+            textBox13.Name = "textBox13";
+            textBox13.Size = new Size(258, 27);
+            textBox13.TabIndex = 28;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(12, 70);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(258, 27);
+            textBox6.TabIndex = 29;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 149);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(258, 27);
+            textBox1.TabIndex = 30;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(12, 217);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(258, 27);
+            textBox5.TabIndex = 31;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(12, 307);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(258, 27);
+            textBox7.TabIndex = 32;
+            // 
+            // RegistrasiFrom
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(860, 450);
+            Controls.Add(textBox7);
+            Controls.Add(textBox5);
+            Controls.Add(textBox1);
+            Controls.Add(textBox6);
+            Controls.Add(textBox13);
+            Controls.Add(textBox10);
+            Controls.Add(textBox9);
+            Controls.Add(textBox4);
+            Controls.Add(textBox12);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(label10);
+            Controls.Add(label11);
+            Controls.Add(label12);
+            Controls.Add(label7);
+            Controls.Add(label8);
+            Controls.Add(label9);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            Name = "RegistrasiFrom";
+            Text = "RegistrasiFrom";
+            Load += RegistrasiFrom_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox12;
+        private ContextMenuStrip contextMenuStrip1;
+        private TextBox textBox4;
+        private TextBox textBox9;
+        private TextBox textBox10;
+        private TextBox textBox13;
+        private TextBox textBox6;
+        private TextBox textBox1;
+        private TextBox textBox5;
+        private TextBox textBox7;
+    }
+}
---
+++ Forms/RegistrasiFrom.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KoperasiBadBoy.Forms
+{
+    public partial class RegistrasiFrom : Form
+    {
+        public RegistrasiFrom()
+        {
+            InitializeComponent();
+        }
+
+        private void RegistrasiFrom_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox9_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
