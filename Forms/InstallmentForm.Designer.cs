be1fb9cac21df090667f4c4be70f3ea099040f66
elhannnn <agit02394@gmail.com>
2025-10-11 09:51:25 +0700

    ..

--- Forms/InstallmentForm.Designer.cs
+++ Forms/InstallmentForm.Designer.cs
@@ -114,7 +114,7 @@
             btnBrowse.Name = "btnBrowse";
             btnBrowse.Size = new Size(112, 34);
             btnBrowse.TabIndex = 8;
-            btnBrowse.Text = "Browse";
+            btnBrowse.Text = "Submit";
             btnBrowse.UseVisualStyleBackColor = true;
             // 
             // btnNewInstallment
@@ -143,6 +143,7 @@
             Controls.Add(dataGridViewInstallment);
             Name = "InstallmentForm";
             Text = "InstallmentForm";
+            Load += InstallmentForm_Load_1;
             ((System.ComponentModel.ISupportInitialize)dataGridViewInstallment).EndInit();
             ResumeLayout(false);
             PerformLayout();
